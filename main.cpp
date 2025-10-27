23fe35027b47fde23f864415443b2962e91da1dd
hackerriddhi <b24cs1063@iitj.ac.in>
2025-10-28 00:03:09 +0530

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
  int main(){
-  cout << "tassali pakdo yaawwrr!!"
+    cout << "tassali pakdo yaawwrr!!";
+    cout << "hello mic testing";
     return 0;
  }
